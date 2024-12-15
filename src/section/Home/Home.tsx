623b3593856b1e854065a2ac9b89a636696fa9ff
jcaceres <jvcaceres17@gmail.com>
2024-12-14 21:51:36 -0500

    Fix render

--- src/section/Home/Home.tsx
+++ src/section/Home/Home.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent, useEffect, useState, useContext } from 'react';
+import React, { FunctionComponent, useEffect, useState, useContext } from 'react';
 import Paper from '@mui/material/Paper';
 import BasicEchart from '../../components/Echart';
 import Footer from '../../components/Footer/Footer';
@@ -61,7 +61,6 @@ const Home: FunctionComponent = () => {
         request.onerror = () => {
             console.error(`Error al obtener los resultados`)
         }
-        setLoading(false)
     }
 
     const getValue = (key: any) => { // traer los ultimos 6 registros
@@ -112,6 +111,7 @@ const Home: FunctionComponent = () => {
                 console.error(`Error al obtener los resultados`)
             }
         }
+        setLoading(false)
     }
 
     useEffect(() => {
--- src/section/TableConsumption/TableConsumption.tsx
+++ src/section/TableConsumption/TableConsumption.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent, useEffect, useContext, useState } from 'react';
+import React, { FunctionComponent, useEffect, useContext, useState } from 'react';
 import Paper from '@mui/material/Paper';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
@@ -18,6 +18,7 @@ const TableConsumption: FunctionComponent = () => {
     const db: any = useContext(ContextDB);
 
     const [data, setData] = useState<any>([])
+    const [loading, setLoading] = useState<boolean>(true)
 
     const list = () => { // para obtener la ultima key
         const request = db.transaction('consumption')
@@ -32,12 +33,13 @@ const TableConsumption: FunctionComponent = () => {
         request.onerror = () => {
             console.error(`Error al obtener los resultados`)
         }
+        setLoading(false)
     }
 
     const deleteValue = (value: number) => {
         const request = db.transaction("consumption", "readwrite")
-        .objectStore('consumption')
-        .delete(value)
+            .objectStore('consumption')
+            .delete(value)
         request.onsuccess = () => {
             list()
         }
@@ -55,34 +57,39 @@ const TableConsumption: FunctionComponent = () => {
     return (
         <div className='card-Consumption-table'>
             <div className='resumen-table'>
-                <Paper elevation={3} style={{ height: '100%'}}>
+                <Paper elevation={3} style={{ height: '100%' }}>
                     <div className='information-Consumption-table'>
-                        {data.map((value: any) => (
-                            <Card className='card' key={value.id}>
-                                <CardContent className='card-content'>
-                                    <Typography gutterBottom sx={{ color: 'text.secondary', fontSize: 14 }}>
-                                        {value.mes}
-                                    </Typography>
-                                    <Typography variant="h6" component="h6">
-                                        Consumo: {value.consumo} kwh
-                                    </Typography>
-                                    <Typography sx={{ color: 'text.secondary', mb: 0.5 }}>Precio kWh: {value.precio_kwh} pesos</Typography>
-                                    <Typography variant="body2">
-                                        {value.observacion}
-                                    </Typography>
-                                </CardContent>
-                                <CardActions>
-                                    <Button
-                                        className='buttom-table-consumption'
-                                        startIcon={<DeleteIcon />}
-                                        variant="contained"
-                                        onClick={() => deleteValue(value.id)}
-                                    >
-                                        Eliminar
-                                    </Button>
-                                </CardActions>
-                            </Card>
-                        ))}
+                        {!loading && (
+                            <>
+                                {data.map((value: any) => (
+                                    <Card className='card' key={value.id}>
+                                        <CardContent className='card-content'>
+                                            <Typography gutterBottom sx={{ color: 'text.secondary', fontSize: 14 }}>
+                                                {value.mes}
+                                            </Typography>
+                                            <Typography variant="h6" component="h6">
+                                                Consumo: {value.consumo} kwh
+                                            </Typography>
+                                            <Typography sx={{ color: 'text.secondary', mb: 0.5 }}>Precio kWh: {value.precio_kwh} pesos</Typography>
+                                            <Typography variant="body2">
+                                                {value.observacion}
+                                            </Typography>
+                                        </CardContent>
+                                        <CardActions>
+                                            <Button
+                                                className='buttom-table-consumption'
+                                                startIcon={<DeleteIcon />}
+                                                variant="contained"
+                                                onClick={() => deleteValue(value.id)}
+                                            >
+                                                Eliminar
+                                            </Button>
+                                        </CardActions>
+                                    </Card>
+                                ))
+                                }
+                            </>
+                        )}
                     </div>
                 </Paper>
             </div>
